f2745d8a65a469df28e78ba7b41139cf93c07744
ehmed-kino <ehmedaff@gmail.com>
2017-03-21 00:31:23 +0000

    display CheckBox onchange on console

--- src/components/questions/question.js
+++ src/components/questions/question.js
@@ -3,21 +3,24 @@ import './question.css';
 
 
 class Question extends Component {
+  handleClick(e) {
+    console.log(`${e.target.name} is ${e.target.checked ? 'selected' : 'not selected'} `);
+  }
   render() {
     const questions = this.props.questions;
     return (
-      <div className="question">
-         {/*looping through all question state*/}
-          {questions.map((val) => {
+      <div className="questions">
+         {/*looping through all questions state*/}
+          {questions.map((val, i) => {
             return (
-              <form>
+              <form key={i}>
                 <div className="question">
                   <h2>{val.question}</h2>
                 </div>
-                {val.answers.map((answer) => {
+                {val.answers.map((answer, i) => {
                   return (
-                    <div className="answer">
-                      <lable><input name="num1" type="checkbox" />{answer}</lable>
+                    <div className="answer" key={i}>
+                      <lable><input name={answer} type="checkbox"  onClick={this.handleClick}/>{answer}</lable>
                     </div>
                   )
                 })}

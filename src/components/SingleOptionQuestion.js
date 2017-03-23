86ab7fcfffa08b9e9470b335f8a6444e02b4d604
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-03-23 20:05:29 +0000

    delete comments

--- src/components/SingleOptionQuestion.js
+++ src/components/SingleOptionQuestion.js
@@ -19,27 +19,3 @@ export default function SingleOptionQuestion(props) {
     </form>
   );
 }
-
-// import React, { Component } from 'react';
-
-// export default class SingleOptionQuestion extends Component {
-//  render() {
-//    const question = this.props.question.question;
-//    const options = this.props.question.options;
-//    const answer = this.props.whenAnswered;
-//    return (
-//      <form>
-//        <h3>{question}</h3>
-//        {
-//          options.map((option, index) => {
-//            return (
-//              <label htmlFor={index} key={index} >{option}
-//                <input type='radio' name='option' value={option} id={index} onChange={answer} /><br/>
-//              </label>
-//            );
-//          })
-//        }
-//      </form>
-//    );
-//  }
-// }

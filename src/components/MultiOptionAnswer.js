bf6eb6a7e0995a741836fb93184b4b06e9a939d8
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-03-31 11:54:53 +0100

    custom answer other

--- src/components/MultiOptionAnswer.js
+++ src/components/MultiOptionAnswer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 let tempAnswers = [];
+let textAreaValue = '';
+let textAreaValueRequired = false;
 
 function collectingAnswer(event) {
   const eventValue = event.target.value;
+  if (eventValue === 'Other') {
+    return (!textAreaValueRequired);
+  }
   const index = tempAnswers.indexOf(eventValue);
-  index === -1 ? tempAnswers.push(eventValue):tempAnswers.splice(index,1);
+  index === -1 ? tempAnswers.push(eventValue) : tempAnswers.splice(index,1);
   // you can use this one as well
   // const checked = event.target.checked;
   // checked ? tempAnswers.push(eventValue) : tempAnswers.splice(index, 1);
@@ -13,9 +18,25 @@ function collectingAnswer(event) {
   // event.preventDefault();
 }
 
+function collectingTextAreaValue(event) {
+  textAreaValue = event.target.value;
+  console.log(textAreaValue);
+}
+
+function answerChecking() {
+  if ((textAreaValueRequired) && (textAreaValue !== '')) {
+    return (tempAnswers.length < 1)
+  }
+}
+
 function answers() {
+  if (textAreaValue) {
+    tempAnswers.push(textAreaValue);
+  }
   let value = tempAnswers;
   tempAnswers = [];
+  textAreaValue = '';
+  textAreaValueRequired = false;
   return value; 
 }
 
@@ -29,8 +50,14 @@ export default function MultiOptionAnswer(props) {
       {
         options.map((option, index) => {
           return (
+            option !== 'Other' ?          
             <label key={index} >{option}
               <input type='checkbox' name='option' value={option} onChange={collectingAnswer} /><br/>
+            </label>
+            :
+            <label key={index} >{option}
+              <input type='checkbox' name='option' value={option} onChange={collectingAnswer} /><br/>
+              <textarea defaultValue='Please specify:' onChange={collectingTextAreaValue} /><br/>
             </label>
           );
         })

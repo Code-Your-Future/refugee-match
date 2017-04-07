122cfdaa2f58594668c182ad0546af2db83ff7c4
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-04-07 11:57:09 +0100

    start with result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,4 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Result(props) {
-  const 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+  	}
+  }
+  render() {
+    return (
+      <div className="result">
+        the result
+      </div>
+    );
+  }
+}

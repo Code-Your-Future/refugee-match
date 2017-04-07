66974aebf1f29a27762952ea6123f3b957f3fff1
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-04-07 11:51:05 +0100

    set locall host 8080

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ export default class App extends Component {
     // set the state to its defaul value
     if (questionNumber === questions.length) {
       // this should be rendering the result page which is in progress
-      this.PostToAPI('http://localhost:9000/api');
+      this.PostToAPI('http://localhost:8080/api');
       return (
         this.setState(
           {
